b234901a22ef80f6f3659724eec206fa16d8106c
yowainwright <yowainwright@gmail.com>
2022-04-27 17:03:28 -0700

    feat: adds more initial work and structure

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,62 +1,166 @@
 #!/usr/bin/env node
-import { resolve } from 'path'
+import { resolve } from "path";
+// the commend out files will be used to re-write the updated package json
 // import { constants, copyFile, writeFile } from 'fs'
-import { resolve } from 'path'
-import { sync } from 'fast-glob'
-import { Options } from './types'
+import { sync } from "fast-glob";
+import { cosmiconfigSync } from "cosmiconfig";
+import { compare } from "compare-versions";
+import {
+  Appendix,
+  Options,
+  OverridesType,
+  PastoralistJSON,
+  ResolveResolutionOptions,
+  UpdateAppendixOptions,
+} from "./types";
+
+export function resolveJSON(path: string): PastoralistJSON {
+  const jsonPath = resolve(path);
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  return require(jsonPath);
+}
 
 /**
- *
- * @param
- * @returns
+ * resolveConfig
+ * @description massages options and config to return all options via CLI args or a config
+ * @param {Options}
+ * @returns {Options}
  */
-export async function resolveConfig({ options }) {
-	const { config: defaultConfig = {} } = explorer.search() || {}
-	const config = options?.config || defaultConfig
-
-	return { config, ...options }
+export function resolveConfig<T extends { options: Options }>({
+  options,
+}: T): Options {
+  const explorer = cosmiconfigSync("pastoralist");
+  const { config: defaultConfig = {} } = explorer.search() || {};
+  const config = options?.config || defaultConfig;
+  return { ...config, ...options };
 }
 
-
-export function generatePackageOverridesList = ({ overridesList }) {
-  return overridesList.reduce((list, overriddenItem) => {
-      if (dependenciesList.includes(overriddenItem)) {
-        list = {
-					...list,
-					[overriddenItem]: {
-						...acc[overriddenItem],
-						[name]: [version],
-					},
-				}
-			}
-      return list
-    }, {})
+/**
+ * resolveOverrides
+ * @description returns an object of overrides and resolutions
+ * @param {Options.path}
+ * @returns {OverridesObject}
+ * @notes
+ * npm overrides return a spec which is readable to what is being overridden
+ * pnpm.overrides & yarn resolutions return "resolutions" which require a readable spec
+ * @example npm overrides spec
+ * https://docs.npmjs.com/cli/v8/configuring-npm/package-json#overrides
+ * ```js
+ * "overrides": {
+ *   "foo": "1.0.0"
+ * }
+ * ```
+ * @example yarn resolutions spec
+ * https://classic.yarnpkg.com/lang/en/docs/selective-version-resolutions/
+ * https://yarnpkg.com/configuration/manifest/#resolutions
+ * ```js
+ * "resolutions": {
+ *  "foo": "1.0.0"
+ * }
+ * ```
+ * @example pnpm.overrides
+ * https://pnpm.io/package_json#pnpmoverrides
+ * ```js
+ * "pnpm": {
+ *   "overrides": {
+ *    "foo": "1.0.0"
+ *   }
+ * }
+ * ```
+ * @warning pastoralist is not built to support npm's nested overrides
+ */
+export function resolveResolutions({
+  config: {
+    overrides: npmOverrides = {},
+    pnpm: { overrides: pnpmOverrides = {} } = {},
+    resolutions = {},
+  } = {},
+  options: { debug = false } = {},
+}: ResolveResolutionOptions): OverridesType {
+  const overridesItems = Object.keys(npmOverrides);
+  const hasOverrides = overridesItems.length > 0;
+  const hasComplexOverrides =
+    hasOverrides &&
+    overridesItems.some((name) => typeof npmOverrides[name] === "object");
+  if (hasComplexOverrides && debug) {
+    console.warn(
+      "🐑 👩🏽‍🌾 Pastoralist only supports simple overrides! Pastoralist is bypassing the specified complex overrides. 👌"
+    );
+  }
+  const overrides = hasOverrides
+    ? overridesItems
+        .filter((name) => typeof npmOverrides[name] === "string")
+        .reduce((acc, name) => ({ ...acc, [name]: npmOverrides[name] }), {})
+    : {};
+  return {
+    ...overrides,
+    ...pnpmOverrides,
+    ...resolutions,
+  };
 }
 
-export function generateOverridesList() {
-  const mainPackageJSON = resolve('package.json')
-  const mainJSON = require(mainPackageJSON)
-  const overrides = Object.assign({}, mainJSON?.overrides, mainJSON?.resolutions)
-  const overridesList = Object.keys(overrides)
-  const nodeModulePackageJSONs = sync(['node_modules/**/package.json'])
-
-  const overridesList = nodeModulePackageJSONs.reduce((acc, packageJSON) => {
-    const { dependencies = {}, name, version } = require(resolve(packageJSON)) || {}
-    const dependenciesList = Object.keys(dependencies)
-    if (!dependenciesList.length) return acc
-    const hasOverriddenDependencies = dependenciesList.some(dependencyItem => overridesList.includes(dependencyItem))
-    if (!hasOverriddenDependencies) return acc
-    const overriddenDependents = generateSinglePackageOverriddenDependents({ overridesList });
-    acc = {
-      ...acc,
-      ...overriddenDependents,
+export function updateAppendix({
+  dependencies,
+  resolutions,
+  name,
+  version,
+  appendix = {},
+}: UpdateAppendixOptions): Appendix {
+  const dependencyList = Object.keys(dependencies);
+  const resolutionsList = Object.keys(resolutions);
+  return resolutionsList.reduce((acc, resolution) => {
+    if (!dependencyList.includes(resolution)) {
+      const hasResolutionOverride = compare(
+        resolutions[resolution],
+        dependencies[resolution],
+        ">"
+      );
+      if (hasResolutionOverride) {
+        return {
+          ...appendix,
+          ...acc,
+          [`${resolution}@${resolutions[resolution]}`]: {
+            ...appendix[resolution],
+            [name]: [version],
+          },
+        };
+      }
     }
-    return acc
-  }, {})
-
-  return overridesList
+    return acc || {};
+  }, {});
 }
 
-export function auditOverrides() {
+export function update(options: Options): Appendix {
+  const config = resolveJSON(options?.path || "package.json");
+  const resolutions = resolveResolutions({ options, config });
+  const resolutionsList = Object.keys(resolutions);
+  const nodeModulePackageJSONs = sync(["node_modules/**/package.json"]);
+  const appendix = nodeModulePackageJSONs.reduce((acc, packageJSON) => {
+    const { dependencies = {}, name, version } = resolveJSON(packageJSON);
+    const dependenciesList = Object.keys(dependencies);
+    if (!dependenciesList.length) return acc;
+    const hasOverriddenDependencies = dependenciesList.some((dependencyItem) =>
+      resolutionsList.includes(dependencyItem)
+    );
+    if (!hasOverriddenDependencies) return acc;
+    const appendixItem = updateAppendix({
+      dependencies,
+      name,
+      resolutions,
+      version,
+      ...(options.appendix ? { appendix: options.appendix } : {}),
+    });
+    return {
+      ...acc,
+      ...appendixItem,
+    };
+  }, {});
 
+  /**
+   * @note review the appendix
+   * These scripts currently work one way => make an appendix
+   * @todo the appendix should manage it self + provide resolutions that are no longer needed
+   */
+
+  return appendix;
 }

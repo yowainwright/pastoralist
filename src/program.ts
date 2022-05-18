52ff9dcd720c767a917747d4629e147542262126
yowainwright <yowainwright@gmail.com>
2022-05-18 10:24:30 -0700

    fix: updates deploy setup

--- src/program.ts
+++ src/program.ts
@@ -23,13 +23,8 @@ export async function action(options: Options = {}) {
 program
   .version(version)
   .description("Pastoralist, a utility CLI to manage your dependency overrides")
-  .option("-p, --path <path>", "the path to the package.json file to manage")
-  .option(
-    "-d, --depPaths <depPaths>",
-    "a path to dependency package.json files"
-  )
   .option("-t, --isTestingCLI", "enables CLI testing, no scripts are run")
-  .parse(process.argv)
-  .action(action as any);
+  .action(action as any)
+  .parse(process.argv);
 
-export default program;
+export { program };

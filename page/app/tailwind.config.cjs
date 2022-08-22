448ca1332e0d3cde17fe6f151986765c3482f226
yowainwright <yowainwright@gmail.com>
2022-08-21 23:16:29 -0700

    feat: updates files

--- page/app/tailwind.config.cjs
+++ page/app/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "./src/**/*.{cjs,ts,js,tsx,mdx}", 'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
+  content: ["index.html", "./src/**/*.{ts,tsx,mdx}", 'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
   daisyui: {
     darkTheme: "acid",
   },

9a76eb8c79509effcb7462f4239122a1d9d0df9c
yowainwright <yowainwright@gmail.com>
2022-08-23 00:34:31 -0700

    feat: adds more styles to app

--- page/app/src/components/Drawer.tsx
+++ page/app/src/components/Drawer.tsx
@@ -1,32 +1,19 @@
 import React, { useState } from 'react';
 import { Button, Drawer as DaisyDrawer } from 'react-daisyui';
+import { SideBar } from './SideBar';
 
-
-const SideItems = (
-  <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-    <li>
-      <a>Sidebar Item 1</a>
-    </li>
-    <li>
-      <a>Sidebar Item 2</a>
-    </li>
-  </ul>
-)
-
-export const Drawer = () => {
+export const Drawer = ({ children }: any) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDrawer= () => {
-    console.log({ isOpen});
-    setIsOpen(!isOpen);
-  }
+  const toggleDrawer= () => setIsOpen(!isOpen);
 
   return (
-    <DaisyDrawer side={SideItems} >
+    <DaisyDrawer side={<SideBar />} open={isOpen} onClickOverlay={toggleDrawer} mobile>
       <div className="flex h-56 items-center justify-center">
         <Button color="primary" className="lg:hidden" onClick={toggleDrawer}>
           Open drawer
         </Button>
+        {children}
       </div>
     </DaisyDrawer>
   );

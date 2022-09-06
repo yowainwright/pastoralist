133a46db9f1d336a6c6faf5d2268dd294bf68daa
yowainwright <yowainwright@gmail.com>
2022-09-05 22:32:30 -0700

    feat: adds working nav/sidebar toggle

--- page/app/src/components/Drawer.tsx
+++ page/app/src/components/Drawer.tsx
@@ -1,20 +1,15 @@
-import React, { useState } from 'react';
-import { Button, Drawer as DaisyDrawer } from 'react-daisyui';
-import { SideBar } from './SideBar';
+import { Drawer as DaisyDrawer } from "react-daisyui";
+import { SideBar } from "./SideBar";
 
-export const Drawer = ({ children }: any) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDrawer= () => {
-    console.log({ isOpen });
-    setIsOpen(!isOpen);
-  }
-
+export const Drawer = ({ children, isOpen, handler }: any) => {
   return (
-    <DaisyDrawer side={<SideBar />} open={isOpen} onClickOverlay={toggleDrawer} mobile>
-      <div className="h-56 px-6">
-        {children}
-      </div>
+    <DaisyDrawer
+      side={<SideBar />}
+      open={isOpen}
+      onClickOverlay={() => handler({ isOpen })}
+      mobile
+    >
+      <div className="h-56 px-6">{children}</div>
     </DaisyDrawer>
   );
-}
+};

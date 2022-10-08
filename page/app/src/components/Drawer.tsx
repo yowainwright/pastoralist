b3776c3c1e5028025a5954e64f95f1e0cf9f7095
yowainwright <yowainwright@gmail.com>
2022-10-08 14:16:58 -0700

    feat: init darkmode

--- page/app/src/components/Drawer.tsx
+++ page/app/src/components/Drawer.tsx
@@ -1,12 +1,12 @@
 import { Drawer as DaisyDrawer } from "react-daisyui";
 import { SideBar } from "./SideBar";
 
-export const Drawer = ({ children, isOpen, handler }: any) => {
+export const Drawer = ({ children, isOpen, toggleDrawer }: any) => {
   return (
     <DaisyDrawer
       side={<SideBar />}
       open={isOpen}
-      onClickOverlay={() => handler({ isOpen })}
+      onClickOverlay={toggleDrawer}
       mobile
     >
       <div className="h-100 px-6">{children}</div>

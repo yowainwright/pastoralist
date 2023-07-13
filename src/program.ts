d23a4aa73a17592db48bbe2fa27e3d765b772cba
Jeff Wainwright <yowainwright@gmail.com>
2023-07-13 15:31:07 -0700

    fix: adds more cli options for debugging

--- src/program.ts
+++ src/program.ts
@@ -3,26 +3,26 @@
 import { program } from "commander";
 import { Options } from "./interfaces";
 import { update } from "./scripts";
-const version = "VERSION";
 
-/**
- * action
- * @description the pastoralist runner
- * @param {Options} Record}
- */
 export async function action(options: Options = {}): Promise<void> {
   try {
-    if (options?.isTestingCLI) console.info({ options });
-
-    update(options);
+    const { debug, depPaths, isTestingCLI = false, path } = options;
+    if (isTestingCLI) {
+      console.info({ options });
+      return
+    }
+    update({ debug, depPaths, path });
   } catch (err) {
-    console.error(err);
+    console.error({ log: "🐑 👩🏽‍🌾 Pastoralist:cli:action:fn", error: err });
+    process.exit(1)
   }
 }
 
 program
-  .version(version)
   .description("Pastoralist, a utility CLI to manage your dependency overrides")
+  .option("-d, --debug", "enables debug mode")
+  .option('--nodeModulePath', 'specifies a node_module path')
+  .option('--json', 'specifies a json path to read from for `resolutions`')
   .option("-t, --isTestingCLI", "enables CLI testing, no scripts are run")
   .action(action)
   .parse(process.argv);

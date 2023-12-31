40f42cbad222c06684fff4473dcb89ff249f671e
Jeff Wainwright <yowainwright@gmail.com>
2023-12-30 23:46:03 -0800

    fix: updates scripts

--- src/scripts.ts
+++ src/scripts.ts
@@ -6,7 +6,7 @@ import { Appendix, Options, OverridesType } from "./interfaces";
 
 const log = logger({ file: "scripts.ts", isLogging: IS_DEBUGGING });
 
-export async function update(options: Options): Promise<Appendix | void> {
+export const update = async (options: Options): Promise<Appendix | void> => {
   const {
     debug = false,
     depPaths = ["node_modules/**/package.json"],
@@ -15,16 +15,21 @@ export async function update(options: Options): Promise<Appendix | void> {
     exec = execPromise,
   } = options;
   const config = resolveJSON(path, debug);
-  if (!config) return;
+  if (!config) {
+    log.debug("update: no config found");
+    return;
+  }
   const resolutions = resolveResolutions({ options, config }) as OverridesType;
+  const resolutionsList = Object.keys(resolutions);
+  const packageJSONs = sync(depPaths);
   const rootDependencies = {
     ...(config?.dependencies ? config?.dependencies : {}),
     ...(config?.devDependencies ? config.devDependencies : {}),
   };
-  const resolutionsList = Object.keys(resolutions);
-  const packageJSONs = sync(depPaths);
+  log.debug("update: initial items", { resolutions, resolutionsList, packageJSONs, rootDependencies });
   const appendix = await packageJSONs.reduce(async (acc, packageJSON): Promise<Appendix> => {
     const currentPackageJSON = resolveJSON(packageJSON, debug);
+    log.debug("update:currentPackageJSON", { currentPackageJSON });
     if (!currentPackageJSON) return acc;
     const { dependencies = {}, name, version } = currentPackageJSON;
     const dependenciesList = Object.keys(dependencies);
@@ -33,24 +38,33 @@ export async function update(options: Options): Promise<Appendix | void> {
     const hasOverriddenDependencies = dependenciesList.some((dependencyItem) =>
       resolutionsList.includes(dependencyItem)
     );
-    console.log({ hasOverriddenDependencies });
-    if (!hasOverriddenDependencies) return acc;
-    const appendixItem = await updateAppendix({
-      debug,
-      packageJSONs,
-      rootDependencies,
-      dependencies,
-      name,
-      resolutions,
-      version,
-      ...(config?.pastoralist?.appendix ? config.pastoralist.appendix : {}),
-      ...(options.appendix ? { appendix: options.appendix } : {}),
-      exec,
-    });
-    return {
-      ...acc,
-      ...appendixItem,
-    };
+    if (!hasOverriddenDependencies) {
+      log.debug("update:hasOverriddenDependencies", { hasOverriddenDependencies });
+      return acc;
+    }
+    try {
+      const appendixItem = await updateAppendix({
+        debug,
+        packageJSONs,
+        rootDependencies,
+        dependencies,
+        name,
+        resolutions,
+        version,
+        ...(config?.pastoralist?.appendix ? config.pastoralist.appendix : {}),
+        ...(options.appendix ? { appendix: options.appendix } : {}),
+        exec,
+      });
+
+      log.debug("update:appendixItem", { appendixItem });
+      return {
+        ...acc,
+        ...appendixItem,
+      };
+    } catch (err) {
+      log.error("update:appendixItem", { err });
+      return acc;
+    }
   }, Promise.resolve({} as Appendix));
 
   const appendixItems = Object.keys(appendix);
@@ -80,6 +94,7 @@ export async function update(options: Options): Promise<Appendix | void> {
     path,
     updatedResolutions,
   });
+
   if (!isTesting) {
     updatePackageJSON({
       appendix,

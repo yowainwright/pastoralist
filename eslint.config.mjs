2a656979dd8e30b89dedf65ca42b8b4d64304db0
Jeff Wainwright <yowainwright@gmail.com>
2024-10-29 02:21:59 -0700

    chore: adds initial tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,22 @@
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from "eslint-plugin-prettier"
+import eslint from "@eslint/js"
+import tseslint from "typescript-eslint"
 
 export default tseslint.config(
 	{ ignores: ["dist", "*.js", "page/*"] },
-	eslintPluginPrettierRecommended,
 	eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		plugins: {
+			prettier: eslintPluginPrettier,
+		},
+		extends: [
+			"eslint:recommended",
+			"plugin:@typescript-eslint/recommended",
+			"plugin:prettier/recommended",
+		],
+		rules: {
+			"prettier/prettier": "error",
+		},
+	},
 )

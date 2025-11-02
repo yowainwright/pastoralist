700c66c8e4ece9504a621da6faa38157ed8f81f7
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2025-11-01 21:38:51 -0700

    chore(root): simplifies page

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import mermaid from "astro-mermaid";
 
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://jeffry.in",
   base: "/pastoralist",
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), mermaid()],
   trailingSlash: "never",
 
   vite: {

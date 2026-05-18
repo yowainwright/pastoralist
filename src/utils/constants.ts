9b3d4217561b7622cc79a1d720741eb14ddc1bbe
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2026-05-17 22:15:12 -0700

    refactor(utils): split glob traversal

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,5 +4,6 @@ export const GLOB_SINGLE_STAR = /\*/g;
 export const GLOB_QUESTION_MARK = /\?/g;
 export const GLOBSTAR_PLACEHOLDER = "{{GLOBSTAR}}";
 export const GLOBSTAR_PLACEHOLDER_PATTERN = /{{GLOBSTAR}}/g;
+export const GLOB_REGEX_CACHE_MAX_SIZE = 200;
 
 export const IGNORED_DIRECTORIES = ["node_modules", ".git"];
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -102,3 +102,44 @@ export interface LoggerOptions {
   file: string;
   isLogging?: boolean;
 }
+
+export interface GlobOptions {
+  cwd?: string;
+  ignore?: string[];
+  absolute?: boolean;
+}
+
+export interface PatternPlan {
+  pattern: string;
+  hasGlobStar: boolean;
+}
+
+export interface DirectMatchContext {
+  cwd: string;
+  ignorePatterns: string[];
+}
+
+export interface DirectMatchPlan {
+  root: string;
+  remainingSegments: string[];
+}
+
+export interface DirectMatchStep {
+  segment: string;
+  isLast: boolean;
+}
+
+export interface DirectMatchState {
+  candidates: string[];
+  results: string[];
+}
+
+export type DirectMatchItem =
+  | {
+      type: "candidate";
+      path: string;
+    }
+  | {
+      type: "result";
+      path: string;
+    };
